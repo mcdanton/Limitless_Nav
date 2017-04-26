68e0eedd6ac4b12696b4dcdd3ced96b9941118fc
mcdanton <danhefter@gmail.com>
2017-04-26 15:22:28 -0400

    Cleaned up code and added Pagination in

--- InfiniteScrolling/InfiniteScrolling/ScrollView.swift
+++ InfiniteScrolling/InfiniteScrolling/ScrollView.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ScrollView: UIViewController, UIGestureRecognizerDelegate {
+class ScrollView: UIViewController, UIGestureRecognizerDelegate, UIScrollViewDelegate {
    
    // MARK: Outlets
    @IBOutlet var superView: UIView!
    @IBOutlet weak var scrollView: UIScrollView!
    @IBOutlet weak var contentView: UIView!
-   
+   @IBOutlet weak var pageControl: UIPageControl!
    
    // MARK: Properties
    var currentView: UIView!
@@ -25,29 +25,19 @@ class ScrollView: UIViewController, UIGestureRecognizerDelegate {
    override func viewDidLoad() {
       super.viewDidLoad()
       
-      currentView = UIView(frame: CGRect(x: 0, y: 0, width: contentView.frame.width, height: contentView.frame.height))
-      currentView.translatesAutoresizingMaskIntoConstraints = false
-      currentView.backgroundColor = UIColor.red
-      contentView.addSubview(currentView)
+      scrollView.delegate = self
       
-      newView = UIView(frame: CGRect(x: currentView.frame.origin.x + currentView.frame.width, y: currentView.frame.origin.y, width: currentView.frame.width, height: currentView.frame.height))
-      newView.backgroundColor = UIColor.green
-      contentView.addSubview(newView)
+      setupInitialSlides()
       
-      scrollView.contentSize = CGSize(width: contentView.frame.width * totalNumberOfPages(), height: view.frame.height)
+      scrollView.contentSize = CGSize(width: contentView.frame.width * CGFloat(totalNumberOfPages()), height: view.frame.height)
       
-
-   
-
-      let longPress = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
-      longPress.minimumPressDuration = 0.01
-      longPress.delegate = self
-      self.view.addGestureRecognizer(longPress)
+      scrollView.isPagingEnabled = true
+      pageControl.numberOfPages = totalNumberOfPages()
+      pageControl.currentPage = 0
       
-      let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(switchScreens))
-      swipeLeft.direction = .left
-      swipeLeft.delegate = self
-      //self.view.addGestureRecognizer(swipeLeft)
+      view.bringSubview(toFront: pageControl)
+      
+      addGestures()
    }
    
    
@@ -71,9 +61,33 @@ class ScrollView: UIViewController, UIGestureRecognizerDelegate {
       } else if gestureRecognizer.state == .ended {
          print("press ended")
          
-//         newView?.removeFromSuperview()
      contentView.transform = CGAffineTransform(scaleX: 1, y: 1)
       }
+   }
+   
+   func setupInitialSlides() {
+      currentView = UIView(frame: CGRect(x: 0, y: 0, width: contentView.frame.width, height: contentView.frame.height))
+      currentView.translatesAutoresizingMaskIntoConstraints = false
+      currentView.backgroundColor = UIColor.red
+      contentView.addSubview(currentView)
+      
+      newView = UIView(frame: CGRect(x: currentView.frame.origin.x + currentView.frame.width, y: currentView.frame.origin.y, width: currentView.frame.width, height: currentView.frame.height))
+      newView.backgroundColor = UIColor.green
+      contentView.addSubview(newView)
+
+      
+   }
+   
+   func addGestures() {
+      let longPress = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
+      longPress.minimumPressDuration = 0.1
+      longPress.delegate = self
+      self.view.addGestureRecognizer(longPress)
+      
+      let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(switchScreens))
+      swipeLeft.direction = .left
+      swipeLeft.delegate = self
+      //self.view.addGestureRecognizer(swipeLeft)
    }
    
    
@@ -95,20 +109,22 @@ class ScrollView: UIViewController, UIGestureRecognizerDelegate {
       return adjustedSize
    }
    
-   func totalNumberOfPages() -> CGFloat {
+   func totalNumberOfPages() -> Int {
       var numberOfPages = 0
       for _ in contentView.subviews {
          numberOfPages += 1
       }
-      return CGFloat(numberOfPages)
+      return numberOfPages
    }
    
    func addPage() {
       
-      let newPage = UIView(frame: CGRect(x: view.frame.width * totalNumberOfPages(), y: 0, width: view.frame.width, height: view.frame.height))
+      let newPage = UIView(frame: CGRect(x: view.frame.width * CGFloat(totalNumberOfPages()), y: 0, width: view.frame.width, height: view.frame.height))
       newPage.backgroundColor = UIColor.random()
       contentView.addSubview(newPage)
-      scrollView.contentSize = CGSize(width: contentView.frame.width * totalNumberOfPages(), height: view.frame.height)
+      scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(totalNumberOfPages()), height: view.frame.height)
+      pageControl.numberOfPages = totalNumberOfPages()
+
    }
    
    func removePage() {
@@ -122,13 +138,17 @@ class ScrollView: UIViewController, UIGestureRecognizerDelegate {
          let alertAction = UIAlertAction(title: "Can't Delete", style: .cancel, handler: nil)
          alertController.addAction(alertAction)
       }
-      
-      
    }
 
    
    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
       return true
+   }
+   
+   func scrollViewDidScroll(_ scrollView: UIScrollView) {
+      
+      let pageIndex = round(scrollView.contentOffset.x / view.frame.width)
+      pageControl.currentPage = Int(pageIndex)
    }
    
 }
